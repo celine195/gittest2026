d1fab062c77d7316f75b19fb85b6a39e80e49c13
celine <11335064@dcsh.tp.edu.tw>
2026-05-22 12:01:06 +0800

    今日預約

--- show/views.py
+++ show/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render, redirect
 from .models import *  
-from .models import Borrowlist
+from .models import Borrowlist, ReservationSchedule  
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.utils import timezone
 from .form import ReservationForm, DeviceForm, TimeForm
+
 
 def reservation_create(request):
     if request.method == "POST":
@@ -36,9 +41,50 @@ def view_borrow_list(request):
     頁面：管理員初核頁面
     功能：查看所有教師送出的借用申請資料
     """
-    # 根據需求文件，由近到遠撈出所有申請，並預先載入對應的教師(user)資料以利效能優化
     borrow_records = Borrowlist.objects.all().order_by('-id')
     
     return render(request, 'borrow_list.html', {
         'borrow_records': borrow_records
     })
+
+
+def today_reservation_list(request):
+
+    today = timezone.localdate()
+
+    today_records = ReservationSchedule.objects.filter(date=today).order_by('period')
+    
+
+    return render(request, 'today_reservation.html', {
+        'today_records': today_records,
+        'today': today
+    })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                
+
+                if hasattr(user, 'role') and user.role == 'admin':
+                    return redirect('today_reservation')  
+                else:
+                    return redirect('view_borrow_list')  
+            else:
+                messages.error(request, "帳號或密碼錯誤")
+        else:
+            messages.error(request, "請輸入正確的帳號密碼")
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  
